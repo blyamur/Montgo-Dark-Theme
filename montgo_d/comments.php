70d1d6bbdf9f25db2210d43309cc5b128cc91310
Monseg <409434+blyamur@users.noreply.github.com>
2021-05-15 09:14:50 +0300

    Update comments.php

--- montgo_d/comments.php
+++ montgo_d/comments.php
@@ -14,8 +14,7 @@ if ( post_password_required() ) {
 			<?php
 				comments_number( '', esc_html__( 'Comment 1 ', 'mo' ), esc_html__( 'Comments % ', 'mo' ) );
 			?>
-		</h2>
-        
+		</h2>     
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-above" class="comment-navigation">
 			<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mo' ); ?></h3>
@@ -25,7 +24,6 @@ if ( post_password_required() ) {
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-above -->
 		<?php endif; // Check for comment navigation. ?>
-
 		<ol class="comment-list">
 			<?php
 				wp_list_comments( array(
@@ -35,7 +33,6 @@ if ( post_password_required() ) {
 				) );
 			?>
 		</ol><!-- .comment-list -->
-
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 		<nav id="comment-nav-below" class="comment-navigation">
 			<h3 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mo' ); ?></h3>
@@ -43,13 +40,11 @@ if ( post_password_required() ) {
 
 				<div class="nav-previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'mo' ) ); ?></div>
 				<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'mo' ) ); ?></div>
-
 			</div><!-- .nav-links -->
 		</nav><!-- #comment-nav-below -->
 		<?php endif; // Check for comment navigation. ?>
 
 	<?php endif; // Check for have_comments(). ?>
-
 	<?php
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
